8ee1c77b752bd6e312273d3a58f6d9b8bd93b97d
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2024-01-01 22:06:03 -0300

    se añadió un nuevo comentario

---
+++ src/programa1.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : programa1.cpp
+// Author      : Thomas Alvarez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int y[10];
+	     cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	y[0] = 2*5*5+3*5+7; //ORDEN DE EJECUCIÓN: 10*5+3*5+7
+	                                     // 50+3*5+7
+	                                     // 50+15+7
+	                                     // 72
+	     cout << "Ingrese un numero entero mayor o igual a 10: ";
+	 cin >> y[1];
+	if(y[1]>=10)
+	  cout << "El resultado de 2*5*5+3*5+7 + "<< y[1]<< " es: "<< y[0] + y[1] << endl;
+	else
+		cout << "El numero ingresado es menor a 10, entonces su numero por defecto es: "<< y[0]<< endl;
+	cout << "\n\n\n\n\n" << "hi"; //crea varios saltos de línea para limpiar y separar la consola
+
+	return 0;
+}
