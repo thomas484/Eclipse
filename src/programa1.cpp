b4b1f4f009fa74c25fc0238013000fbddd662959
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2024-03-18 02:25:59 -0300

    programa modificado para sacar promedio

--- src/programa1.cpp
+++ src/programa1.cpp
@@ -10,17 +10,19 @@
 using namespace std;
 
 int main() {
-	int y[10];
-	char a;
+	int notas[10];
+	float promedio;
+	//char a;
 	     cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	y[0] = 2*5*5+3*5+7; //ORDEN DE EJECUCIÓN: 10*5+3*5+7
-	                                     // 50+3*5+7
-	                                     // 50+15+7
-	                                     // 72
-	     cout << "Ingrese un numero entero mayor o igual a 10: ";
-	 cin >> y[1];
-	if(y[1]>=10)
-	  cout << "El resultado de 2*5*5+3*5+7 + "<< y[1]<< " es: "<< y[0] + y[1] << endl;
+	     for (int i = 0;i<3;i++){
+	     cout << "Ingrese la nota n°"<<(i+1)<< ": ";
+	     cin >> notas[i];
+	     }
+	     promedio = (notas[0]+notas[1]+notas[2])/3;
+	     cout << "Su promedio es de: "<< promedio << endl;
+
+	/*if(y[0]>0)
+	  cout << "El resultado de "<< y[1]<< " es: "<< y[0] + y[1] << endl;
 	else
 		cout << "El numero ingresado es menor a 10, entonces su numero por defecto es: "<< y[0]<< endl;
 	cout << "\n\n\n\n\n" << "hi" << endl ; //crea varios saltos de línea para limpiar y separar la consola
@@ -28,6 +30,6 @@ int main() {
 	cout << "Ingrese cualquier caracter de un solo digito: ";
 	cin >> a;
 	cout << "El equivalente a un entero del caracter es: "<< static_cast<int>(a) << endl; //usa un cast de char a entero
-
+*/
 	return 0;
 }
